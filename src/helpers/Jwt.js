063478b5ff9c47e3fd0ab14456cf1fe73af9aad9
Farshad Abbasi <farshad.abbasi.nejhad@gmail.com>
2019-06-26 11:20:33 +0430

    add cookie and edit git ignore

--- src/helpers/Jwt.js
+++ src/helpers/Jwt.js
@@ -1,13 +1,21 @@
-﻿let key='acc_token'
-export const setJwt =(token) =>
-{
-    
-    localStorage.setItem(key,token);
-}
-export  const getJwt =   () =>{
-    console.log(localStorage.getItem(key));
-      return localStorage.getItem(key);
-}
-export const rmJwt = ()=> {
-    localStorage.removeItem(key)
-}
+﻿import Cookies from "js-cookie";
+let key = "acc_token";
+
+export const setJwt = token => {
+  //localStorage.setItem(key, token);
+  //document.cookie = "acc_token" + "=" + token;
+
+  Cookies.set(key, token, { expires: 1 });
+  //cookie.set(key, token);
+  //setCookie("acc_token", token, { path: "/" });
+};
+export const getJwt = () => {
+  let cookie = Cookies.get(key);
+  console.log(cookie);
+  return cookie;
+  //return localStorage.getItem(key);
+};
+export const rmJwt = () => {
+  Cookies.remove(key);
+  //localStorage.removeItem(key);
+};
