530768ff7046d4cb013a6615ac09a8f305a1478a
reza bashiri <rzabshiri@gmail.com>
2019-07-09 21:34:01 +0430

    update login page to show error

--- src/routes/layouts/login.js
+++ src/routes/layouts/login.js
@@ -1,47 +1,39 @@
-import React, { Component, Fragment } from "react";
-import IntlMessages from "Util/IntlMessages";
-import { Row, Card, CardTitle, Form, Label, Input } from "reactstrap";
-import Button from "reactstrap-button-loader";
-import { NavLink } from "react-router-dom";
+import React, { Component, Fragment } from 'react';
+import IntlMessages from 'Util/IntlMessages';
+import { Row, Card, CardTitle, Form, Label, Input } from 'reactstrap';
+import Button from 'reactstrap-button-loader';
+import { NavLink } from 'react-router-dom';
 
-import { Colxx } from "Components/CustomBootstrap";
+import { Colxx } from 'Components/CustomBootstrap';
 
-import { connect } from "react-redux";
-import { loginUser } from "Redux/actions";
+import { connect } from 'react-redux';
+import { loginUser } from 'Redux/actions';
 
-import userService from "../../services/userService.jsx";
-import userModel from "../../models/userModel.jsx";
-import { getJwt } from "../../helpers/Jwt.js";
-import swal from "sweetalert";
+import userService from '../../services/userService.jsx';
+import userModel from '../../models/userModel.jsx';
+import { getJwt } from '../../helpers/Jwt.js';
+import swal from 'sweetalert';
 import {
   AvForm,
   AvGroup,
   AvInput,
   AvFeedback,
   AvField
-} from "availity-reactstrap-validation";
+} from 'availity-reactstrap-validation';
 import {
   mobileValidation,
   passwordValidation
-} from "../../constants/validations";
+} from '../../constants/validations';
 
 class LoginLayout extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: "",
-      password: "",
+      email: '',
+      password: '',
       loading: 0
     };
   }
-  /*
-  onUserLogin() {
-    if (this.state.email !== "" && this.state.password !== "") {
-      this.props.loginUser(this.state, this.props.history);
-    }
-  }
-  */
-
   async onUserLogin(event, value) {
     event.preventDefault();
     this.setState({
@@ -50,18 +42,16 @@ class LoginLayout extends Component {
     var srv = new userService();
 
     var user = new userModel();
-    /*const data = new FormData(event.target);
-    data.forEach((value, key) => {
-      user[key] = value;
-    });*/
+
     Object.keys(value).map(key => (user[key] = value[key]));
 
     try {
       await srv.getToken(user);
-      if (getJwt() !== null) this.props.history.push("app");
+      if (getJwt() !== null) this.props.history.push('app');
+      else swal('خطا', 'نام کاربری یا رمز عبور معتبر نیست', 'warning');
     } catch (e) {
       console.log(e);
-      swal("خطا", "نام کاربری یا رمز عبور معتبر نیست", "warning");
+      swal('خطا', 'نام کاربری یا رمز عبور معتبر نیست', 'warning');
     } finally {
       this.setState({
         loading: 0
@@ -70,74 +60,74 @@ class LoginLayout extends Component {
   }
 
   componentDidMount() {
-    document.body.classList.add("background");
+    document.body.classList.add('background');
   }
   componentWillUnmount() {
-    document.body.classList.remove("background");
+    document.body.classList.remove('background');
   }
   render() {
     return (
       <Fragment>
-        <div className="fixed-background" />
+        <div className='fixed-background' />
         <main>
-          <div className="container">
-            <Row className="h-100">
-              <Colxx xxs="12" md="10" className="mx-auto my-auto">
-                <Card className="auth-card">
-                  <div className="position-relative image-side ">
-                    <p className="white mb-0">
+          <div className='container'>
+            <Row className='h-100'>
+              <Colxx xxs='12' md='10' className='mx-auto my-auto'>
+                <Card className='auth-card'>
+                  <div className='position-relative image-side '>
+                    <p className='white mb-0'>
                       برای استفاده از امکانات پرسیس وارد شوید
                     </p>
                   </div>
-                  <div className="form-side">
-                    <NavLink to={`/`} className="white">
-                      <span className="logo-single" />
+                  <div className='form-side'>
+                    <NavLink to={`/`} className='white'>
+                      <span className='logo-single' />
                     </NavLink>
-                    <CardTitle className="mb-4">
-                      <IntlMessages id="user.login-title" />
+                    <CardTitle className='mb-4'>
+                      <IntlMessages id='user.login-title' />
                     </CardTitle>
                     <AvForm
                       onValidSubmit={async (e, v) =>
                         await this.onUserLogin(e, v)
                       }
                     >
-                      <Label className="form-group has-float-label mb-4">
+                      <Label className='form-group has-float-label mb-4'>
                         <AvField
-                          name="userName"
-                          id="userName"
+                          name='userName'
+                          id='userName'
                           validate={mobileValidation}
                         />
-                        <IntlMessages id="forms.mobile" />
+                        <IntlMessages id='forms.mobile' />
                       </Label>
-                      <Label className="form-group has-float-label mb-4">
+                      <Label className='form-group has-float-label mb-4'>
                         <AvField
-                          name="password"
-                          id="password"
+                          name='password'
+                          id='password'
                           validate={passwordValidation}
-                          type="password"
+                          type='password'
                         />
                         <IntlMessages
-                          id="user.password"
+                          id='user.password'
                           defaultValue={this.state.password}
                         />
                       </Label>
-                      <div className="d-flex justify-content-between align-items-center">
+                      <div className='d-flex justify-content-between align-items-center'>
                         <NavLink to={`/forgot-password`}>
-                          <IntlMessages id="user.forgot-password-question" />
+                          <IntlMessages id='user.forgot-password-question' />
                         </NavLink>
                         <Button
-                          color="primary"
-                          className="btn-shadow"
-                          size="lg"
-                          type="submit"
+                          color='primary'
+                          className='btn-shadow'
+                          size='lg'
+                          type='submit'
                           loading={this.state.loading}
                         >
-                          <IntlMessages id="user.login-button" />
+                          <IntlMessages id='user.login-button' />
                         </Button>
                       </div>
-                      <div className="d-flex justify-content-between align-items-center">
+                      <div className='d-flex justify-content-between align-items-center'>
                         <NavLink to={`/register`}>
-                          <IntlMessages id="user.register" />
+                          <IntlMessages id='user.register' />
                         </NavLink>
                       </div>
                     </AvForm>
