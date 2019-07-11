f33429f9ee4c1cc78e1574608042d8cea9961846
Farshad Abbasi <farshad.abbasi.nejhad@gmail.com>
2019-07-11 17:09:56 +0430

    edit hostCalendar

--- src/routes/hosts/hostCalendar.js
+++ src/routes/hosts/hostCalendar.js
@@ -3,6 +3,7 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import HostPriceForm from "./hostPriceForm";
 import IntlMessages from "Util/IntlMessages";
+import { Colxx } from "Components/CustomBootstrap";
 import {
   Row,
   Card,
@@ -12,6 +13,7 @@ import {
   ModalBody,
   ModalFooter
 } from "reactstrap";
+import Switch from "rc-switch";
 //import resourceTimelinePlugin from "@fullcalendar/resource-timeline";
 //import resourceTimeGridPlugin from "@fullcalendar/resource-timegrid";
 //import { toMoment } from "@fullcalendar/moment"; // only for formatting
@@ -20,71 +22,64 @@ import interactionPlugin from "@fullcalendar/interaction";
 import esLocale from "@fullcalendar/core/locales/fa";
 
 class HostCalendar extends Component {
+  calendarRef = React.createRef();
   constructor(props) {
     super(props);
     this.togglePriceModal = this.togglePriceModal.bind(this);
 
     this.state = {
       calendarModalOpen: false,
-      calendarEvents: [
-        {
-          title: "روز غیر پیک",
-          start: new Date(),
-          allDay: true,
-          editable: true,
-          resourceEditable: true,
-          resourceId: "a"
-        }
-      ]
+      calendarEvents: [],
+      eventType: "lastMin",
+      lastMinEvent: false,
+      picTimeEvent: true
     };
   }
-  calendarRef = React.createRef();
-  togglePriceModal() {
-    this.setState({
-      priceModalOpen: !this.state.priceModalOpen
-    });
-  }
-  handleDateClick = arg => {
-    this.setState({
-      calendarEvents: this.state.calendarEvents.concat({
-        // creates a new array
-        title: "روز پیک",
-        start: arg.date,
-        allDay: arg.allDay,
-        resourceId: "a",
-        editable: true
-      })
-    });
-    /*this.calendarRef.addEvent({
-      title: "روز غیر پیک",
-      start: new Date(),
-      allDay: true,
-      editable: true,
-      resourceEditable: true
-    });*/
-  };
-  onDataClick = arg => {
-    this.setState({
-      priceModalOpen: !this.state.priceModalOpen
-    });
-  };
 
   render() {
     return (
       <Fragment>
+        <Row className="mb-4">
+          <Colxx xxs="4">
+            <label>
+              <IntlMessages id="تخفیف لحظه آخر" />
+            </label>
+            <Switch
+              className="custom-switch custom-switch-primary"
+              checked={this.state.lastMinEvent}
+              onChange={lastMinEvent => {
+                this.setState({
+                  lastMinEvent: lastMinEvent,
+                  picTimeEvent: !this.state.picTimeEvent
+                });
+              }}
+            />
+          </Colxx>
+          <Colxx xxs="4">
+            <label>
+              <IntlMessages id="روز پیک" />
+            </label>
+            <Switch
+              className="custom-switch custom-switch-primary"
+              checked={this.state.picTimeEvent}
+              onChange={picTimeEvent => {
+                this.setState({
+                  picTimeEvent: picTimeEvent,
+                  lastMinEvent: !this.state.lastMinEvent
+                });
+              }}
+            />
+          </Colxx>
+        </Row>
+
         <FullCalendar
           defaultView="dayGridMonth"
           plugins={[dayGridPlugin, interactionPlugin]}
           locale={esLocale}
           timeZone="Asia/Tehran"
-          resources={[
-            {
-              id: "a",
-              title: "Room A"
-            }
-          ]}
           events={this.state.calendarEvents}
           dateClick={this.handleDateClick}
+          selectable={true}
           ref={this.calendarRef}
         />
         <Modal
@@ -102,6 +97,59 @@ class HostCalendar extends Component {
       </Fragment>
     );
   }
+  togglePriceModal() {
+    this.setState({
+      priceModalOpen: !this.state.priceModalOpen
+    });
+  }
+  handlePicTime = (arg, calendarApi) => {
+    let todayEvent = calendarApi.getEventById(arg.dateStr + "-picTime");
+    if (todayEvent !== null) {
+      todayEvent.remove();
+    } else {
+      calendarApi.addEvent({
+        id: arg.dateStr + "-picTime",
+        title: "روز پیک",
+        start: arg.date,
+        allDay: true,
+        editable: true,
+        resourceEditable: true
+      });
+    }
+  };
+  handleLastMin = (arg, calendarApi) => {
+    let todayEvent = calendarApi.getEventById(arg.dateStr + "-lastMin");
+    if (todayEvent !== null) {
+      todayEvent.remove();
+    } else {
+      calendarApi.addEvent({
+        id: arg.dateStr + "-lastMin",
+        title: "لحظه آخری",
+        start: arg.date,
+        allDay: true,
+        editable: true,
+        resourceEditable: true
+      });
+    }
+  };
+
+  handleDateClick = arg => {
+    let calendarApi = this.calendarRef.current.getApi();
+    if (this.state.lastMinEvent) {
+      this.handleLastMin(arg, calendarApi);
+    }
+    if (this.state.picTimeEvent) {
+      this.handlePicTime(arg, calendarApi);
+    }
+
+    //console.log(calendarApi.getEvents());
+    //console.log(this.state.eventType);
+  };
+  onDataClick = arg => {
+    this.setState({
+      priceModalOpen: !this.state.priceModalOpen
+    });
+  };
 }
 
 export default HostCalendar;
